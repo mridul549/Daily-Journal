b246ccf9f6d68579a36256e79beb3f787f3c9179
Mridul Verma <starship@192.168.1.9>
2022-12-20 16:19:29 +0530

    Version 2

--- app.js
+++ app.js
@@ -54,6 +54,6 @@ app.post("/compose",function(req,res){
   res.redirect("/");
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started!");
 });
